a15e17f5878de67cd2a2855eed87547ed87eb72b
ItsikD <ItsikDangoor@users.noreply.github.com>
2018-09-17 23:33:23 +0300

    exp folder

---
+++ exp/game.js
@@ -0,0 +1,23 @@
+/*function injectCardsToHTML() {
+    let html = "";
+    for(let i = 1; i <= TOTAL_COUPLES_COUNT; i += 1) {
+        for(let j = 0; j < 2; j += 1) {
+            html += `<div class="card" data-card="${i}">
+                        <img src="img/cards/${i}.png">
+                        <img class="back" src="img/cards/back.png">
+                    </div>
+`;
+        }
+    }
+    return html;
+}*/
+
+
+window.onload = function() {
+    gamerName = promptGamerName();
+    registerGamer(gamerName);
+    //log(injectCardsToHTML());
+    //board.innerHTML = injectCardsToHTML();
+    prepareEventHandlers();
+    shuffleCards();
+};
